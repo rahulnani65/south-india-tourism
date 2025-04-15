1cd6b187f6cacb4d26193b6c85e802592255a8d9
RAHULN6688 <123615771+RAHULN6688@users.noreply.github.com>
2025-04-15 14:07:16 +0530

    Added admin setup

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import State, TouristPlace, Restaurant
 
-# Register your models here.
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']
+    search_fields = ['name']
+
+@admin.register(TouristPlace)
+class TouristPlaceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'state', 'category']
+    list_filter = ['state', 'category']
+    search_fields = ['name']
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'tourist_place', 'dietary_tags']
+    list_filter = ['tourist_place']
+    search_fields = ['name', 'dietary_tags']
